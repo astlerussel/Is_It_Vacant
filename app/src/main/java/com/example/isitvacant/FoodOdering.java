d8aa46f392fe9c675f854eb3d8c153c7728035b5
Chiranthanmv <chiranthanmv@gmail.com>
2020-03-13 00:56:29 +0530

    reviews done

--- app/src/main/java/com/example/isitvacant/FoodOdering.java
+++ app/src/main/java/com/example/isitvacant/FoodOdering.java
@@ -2,14 +2,21 @@ package com.example.isitvacant;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.tabs.TabLayout;
 
 public class FoodOdering extends AppCompatActivity {
@@ -30,6 +37,8 @@ public class FoodOdering extends AppCompatActivity {
     private String mParam4;
 
 
+    BottomSheetDialog dialog;
+    TextView cart,Reserve;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,25 @@ public class FoodOdering extends AppCompatActivity {
         mViewPager.setAdapter(myFoodAccessAdapter);
         mTabLayout = findViewById(R.id.main_tab);
         mTabLayout.setupWithViewPager(mViewPager);
+
+        cart = findViewById(R.id.cart);
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog = new BottomSheetDialog(FoodOdering.this);
+                dialog.setContentView(R.layout.view_cart);
+                Reserve = dialog.findViewById(R.id.Reserve_food);
+                Reserve.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(FoodOdering.this,booking_summary.class));
+                    }
+                });
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
 
 
 
